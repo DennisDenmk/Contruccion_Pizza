8874763c5df2f9ab721f682eab7a870da9ee68a1
Jordan Arcos <jordanarcos0@gmail.com>
2025-07-21 06:09:01 -0500

    Reapply "Maestro_detalle pizzas incompleto"
    This reverts commit 69550329bc461108bdd1f7cd679875dc195a0cde.

--- pizza-api/src/typeDefs/pizza.type.js
+++ pizza-api/src/typeDefs/pizza.type.js
@@ -7,21 +7,44 @@ module.exports = gql`
     piz_state: Boolean!
   }
 
+  input PizzaIngredientInput {
+    ing_id: Int!
+    piz_ing_quantified: Float!
+  }
+
   type Pizza {
     piz_id: ID!
     piz_name: String!
     piz_origin: String!
     piz_state: Boolean!
+    ingredients: [PizzaIngredient]
+  }
+
+  type PizzaIngredient {
+    ing_id: Int!
+    piz_ing_quantified: Float!
+    ingredient: Ingredient
+  }
+
+  type Ingredient {
+    ing_id: Int!
+    ing_name: String!
+    ing_state: Boolean!
+    ing_calories: Float!
   }
 
   type Query {
     pizzas: [Pizza]
     pizza(id: ID!): Pizza
+    ingredients: [Ingredient]
   }
 
   type Mutation {
     crearPizza(input: PizzaInput!): Pizza
     actualizarPizza(id: ID!, input: PizzaInput!): Pizza
     eliminarPizza(id: ID!): Boolean
+    createPizzaWithIngredients(piz_name: String!, piz_origin: String!, piz_state: Boolean!, ingredients: [PizzaIngredientInput!]!): Pizza!
+    updatePizzaWithIngredients(piz_id: ID!, piz_name: String!, piz_origin: String!, piz_state: Boolean!, ingredients: [PizzaIngredientInput!]!): Pizza!
+    deletePizzaWithRelations(piz_id: ID!): Boolean!
   }
 `;
